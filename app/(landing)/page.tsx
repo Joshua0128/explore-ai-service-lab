0386ee81224390f13104c2334de98e04c3ec8fac
Wei Ting Wu <weiting@projectmars.info>
2024-03-25 16:18:02 +0800

    feat:setup the project structure with route group

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const LandingAPage = () => {
+	return (
+		<div>
+			<div>LandingAPage (Unprotected)</div>
+			<Button> Click to login in </Button>
+		</div>
+	)
+}
+
+export default LandingAPage
