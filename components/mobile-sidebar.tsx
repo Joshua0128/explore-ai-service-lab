be7ceaf5d2f96c4ee7b5678280188c95253d8093
Wei Ting Wu <weiting@projectmars.info>
2024-03-25 17:58:52 +0800

    fix: prevent React hydration error and rendering issues
    - Add hydration error check to prevent React hydration error in Next.js
    - Initialize `isMounted` state with `useState` hook
    - Add effect hook to set `isMounted` to true on mount
    - Return `null` if component is not mounted to prevent rendering issues

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,8 +4,21 @@ import { Menu } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { SheetTrigger, Sheet, SheetContent } from '@/components/ui/sheet'
 import Sidebar from '@/components/sidebar'
+import { useEffect, useState } from 'react'
 
 const MobileSideBar = () => {
+	// hydration error
+	// https://stackoverflow.com/questions/73162551/how-to-solve-react-hydration-error-in-nextjs
+	const [isMounted, setIsMounted] = useState(false)
+
+	useEffect(() => {
+		setIsMounted(true)
+	}, [])
+
+	if (!isMounted) {
+		return null
+	}
+
 	return (
 		<Sheet>
 			<SheetTrigger>
